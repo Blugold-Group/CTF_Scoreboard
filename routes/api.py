2a06083b167a5633488a75f55d0d17064aeb9f71
Jack Hagen <jjhags@gmail.com>
2025-12-23 23:13:37 -0600

    Gutted ctf, bug bounty, and accout=nt systems. Now just serves static content

--- routes/api.py
+++ routes/api.py
@@ -1,3 +1,6 @@
+"""
+# This was all written by Silas for the Bluscreen Discord bot when we were hostig CT stuff on the website, if he wants to keep the bot it will need to migrate to use the new ctfd server 
+
 from flask import Blueprint, request, session, flash, redirect, url_for, send_file
 from flask_login import current_user, login_required
 import os, secrets, logging, requests, html
@@ -369,3 +372,5 @@ def update_challenge_order():
         query_db('UPDATE challenge SET order_index = ? WHERE id = ?', (challenge['order_index'], challenge['id']))
 
     return {'success': True, 'message': 'Challenge order updated successfully'}
+
+"""
--- routes/blog.py
+++ routes/blog.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, flash, session, send_from_directory, Blueprint
-from helpers import *
+from flask import Flask, render_template, redirect, url_for, flash, Blueprint
 from config import *
 
 import os
